fc3badd9c056235a22cb9ff51ac84bb9e3a14fcb
Ahmed Kaffel <53304536+ahmed111789@users.noreply.github.com>
2026-06-29 21:22:49 +0100

    Enhance test coverage for add and subtract functions
    Added tests for subtraction and negative addition cases.

--- test_calculator.py
+++ test_calculator.py
@@ -1,4 +1,10 @@
-from calculator import add
+from calculator import *
 
 def test_add():
     assert add(2, 3) == 5
+    assert add(-2, 3) == 1
+
+def test_substract():
+    assert substract(5, 1) == 4
+    assert substract(1, 5) == -4
+    
